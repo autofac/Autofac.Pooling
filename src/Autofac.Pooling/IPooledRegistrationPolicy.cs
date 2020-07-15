3e3130adb1e8351247e16b0f4c724877783123b9
Alistair Evans <alistairjevans@gmail.com>
2020-07-15 08:22:50 +0100

    Improved the comments on IPooledRegistrationPolicy.MaximumRetained.

--- src/Autofac.Pooling/IPooledRegistrationPolicy.cs
+++ src/Autofac.Pooling/IPooledRegistrationPolicy.cs
@@ -13,7 +13,8 @@ namespace Autofac.Pooling
         where TPooledObject : class
     {
         /// <summary>
-        /// Gets a value indicating the maximum number of items that will be retained in the pool.
+        /// Gets a value indicating the maximum number of items that will be retained in the pool. This is not a limit on the number of
+        /// objects that can be created, it's the threshold past which any objects returned to the pool will be discarded..
         /// </summary>
         int MaximumRetained { get; }
 

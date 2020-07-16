516804d87ac70ded649cf2628c0bfe71647aaaa1
Alistair Evans <alistairjevans@gmail.com>
2020-07-16 07:36:23 +0100

    Added tests for unsupported scenarios.

---
+++ test/Autofac.Pooling.Tests/RegistrationExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using Autofac.Builder;
+using Autofac.Pooling.Tests.Shared;
+using System;
+using Xunit;
+
+namespace Autofac.Pooling.Tests
+{
+    public class RegistrationExtensionsTests
+    {
+        [Fact]
+        public void RequiresCallbackContainer()
+        {
+            // Manually create a registration builder, then call AsPooled
+            var regBuilder = RegistrationBuilder.ForType<PooledComponent>();
+
+            Assert.Throws<NotSupportedException>(() => regBuilder.PooledInstancePerLifetimeScope());
+        }
+
+        [Fact]
+        public void NoProvidedInstances()
+        {
+            var builder = new ContainerBuilder();
+
+            var regBuilder = builder.RegisterInstance(new PooledComponent());
+
+            Assert.Throws<NotSupportedException>(() => regBuilder.PooledInstancePerLifetimeScope());
+        }
+
+        [Fact]
+        public void OnReleaseNotCompatible()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<PooledComponent>()
+                   .PooledInstancePerLifetimeScope()
+                   .OnRelease(args => { });
+
+            Assert.Throws<NotSupportedException>(() => builder.Build());
+        }
+    }
+}

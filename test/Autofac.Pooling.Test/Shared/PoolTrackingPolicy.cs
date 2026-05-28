d5b9fe007a4714015a316981f976d485ee16d1e2
Travis Illig <tillig@paraesthesia.com>
2026-05-28 16:22:11 -0700

    Fix CA1062: add null check for parameter validation.

--- test/Autofac.Pooling.Test/Shared/PoolTrackingPolicy.cs
+++ test/Autofac.Pooling.Test/Shared/PoolTrackingPolicy.cs
@@ -14,6 +14,8 @@ public class PoolTrackingPolicy<TLimit> : DefaultPooledRegistrationPolicy<TLimit
 
     public override TLimit Get(IComponentContext context, IEnumerable<Parameter> parameters, Func<TLimit> getFromPool)
     {
+        ArgumentNullException.ThrowIfNull(getFromPool);
+
         Interlocked.Increment(ref _outOfPool);
 
         return getFromPool();
